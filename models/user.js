4338ef260b6aac4ea6362c6fe2d378c3cbbf72bf
Abdurashidjon <abdurashidgaybullayev@gmail.com>
2021-07-22 10:24:11 +0500

    71-amaliy

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+        uniqu: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024,
+    }
+});
+
+const User = mongoose.model('User',userSchema);
+function validateUser(user){
+    const schema = Joi.object({
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(50).required(),
+        password: Joi.string().min(5).max(1024).required()
+    });
+    return schema.validate(user);
+}
+exports.User = User;
+exports.validate = validateUser;
